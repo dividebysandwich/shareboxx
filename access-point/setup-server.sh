663570df36b06426c95411628e98f1a16d3d2c9a
dividebysandwich <josef.jahn@gmail.com>
2024-04-30 15:27:52 +0200

    moved default binary location to /usr/bin

--- access-point/setup-server.sh
+++ access-point/setup-server.sh
@@ -4,7 +4,7 @@ mkdir /shareboxx
 mkdir /shareboxx/files
 chown shareboxx /shareboxx -R
 cp -r ../target/site /shareboxx/
-cp ../target/release/shareboxx /usr/local/bin/
+cp ../target/release/shareboxx /usr/bin/
 cp ./shareboxx.service /etc/systemd/system/
 systemctl enable shareboxx
 systemctl start shareboxx
