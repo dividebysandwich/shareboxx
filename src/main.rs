61413f95a9148a089b4408028490426e92bc45ee
dividebysandwich <josef.jahn@gmail.com>
2024-05-01 11:10:28 +0200

    Changed default URI

--- src/main.rs
+++ src/main.rs
@@ -124,12 +124,12 @@ async fn domain_redirect(
     use actix_web::HttpResponse;
     //Only run this check in release builds
     #[cfg(not(debug_assertions))]
-    // Check if request hostname matches shareboxx, otherwise redirect to it.
-    if req.connection_info().host() != "shareboxx" {
+    // Check if request hostname matches shareboxx.lan, otherwise redirect to it.
+    if req.connection_info().host() != "shareboxxlan" {
         return Ok(ServiceResponse::new(
             req.request().to_owned(),
             HttpResponse::TemporaryRedirect()
-                .append_header(("Location", "https://shareboxx"))
+                .append_header(("Location", "https://shareboxx.lan"))
                 .finish(),
         )
         .map_into_boxed_body());
