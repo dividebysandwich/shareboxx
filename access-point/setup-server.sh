56eb11e5a8e2c4569fc80fb21b4a0bd6be8c32e4
dividebysandwich <josef.jahn@gmail.com>
2024-05-06 19:47:31 +0200

    Fixed scripts for chat

--- access-point/setup-server.sh
+++ access-point/setup-server.sh
@@ -2,6 +2,8 @@
 useradd shareboxx
 mkdir /shareboxx
 mkdir /shareboxx/files
+touch /shareboxx/chat.json
+chmod +w /shareboxx/chat.json
 chown shareboxx /shareboxx -R
 cp -r ../target/site /shareboxx/
 cp ../target/release/shareboxx /usr/bin/
